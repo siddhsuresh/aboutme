b6855ab6e724b0b17cb072cb5e561c651a664e84
Siddharth Suresh <siddharth.suresh2020@vitstudent.ac.in>
2022-09-17 18:28:18 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
+        <time>{YEAR}</time> © Siddharth Suresh.
       </small>
       <style jsx>{`
         footer {
